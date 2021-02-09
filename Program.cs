682ab469dcfda72f72c6cccb6357ccf8300bcfe0
Ash Jones <74986794+DenseTurd@users.noreply.github.com>
2021-02-09 00:09:13 +0000

    V0.Turd
    Very broken, oh well try again some time :)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,80 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Sprache;
+
+// Errrr.....
+
+public static class Alphametics
+{
+    public static IDictionary<char, int> Solve(string equation)
+    {
+        var allAdded = all.Parse(equation);
+        var allChars = allAdded.added.Append(allAdded.summed).SelectMany(o => o).Distinct();
+        var cantBeZero = allAdded.added.Append(allAdded.summed).Select(o => o.First()).Distinct();
+
+        var result = Combinations(allChars, Enumerable.Empty<int>(), cantBeZero)
+            .Select(o => o.ToDictionary(m => m.letter, m => m.value))
+            .FirstOrDefault(m => IsValid(allAdded.added, allAdded.summed, m));
+
+        return result ?? throw new ArgumentException();
+    }
+
+    private static IEnumerable<IEnumerable<(char letter, int value)>> Combinations(IEnumerable<char> remaining, IEnumerable<int> used, IEnumerable<char> cantBeZero)
+    {
+        if (!remaining.Any())
+        {
+            yield return Enumerable.Empty<(char letter, int value)>();
+            yield break;
+        }
+
+        var letter = remaining.First();
+        var start = cantBeZero.Contains(letter) ? 1 : 0;
+
+        foreach (var n in Enumerable.Range(start, 10 - start).Except(used))
+        {
+            var option = (letter, n);
+            foreach (var next in Combinations(remaining.Skip(1), used.Append(n), cantBeZero))
+                yield return next.Append(option);
+        }
+    }
+
+    private static bool IsValid(char[][] added, char[] summed, Dictionary<char, int> mapping)
+    {
+        long asNumber(char[] chars) =>
+            chars.Reverse()
+                .Select((c, i) => mapping.GetValueOrDefault(c) * (long)Math.Pow(10, i))
+                .Sum();
+
+        var targetNumber = asNumber(summed);
+        long sum = 0;
+        foreach (var chars in added)
+        {
+            var number = asNumber(chars);
+            if (number > targetNumber || sum + number > targetNumber)
+                return false;
+            sum += number;
+        }
+        return sum == targetNumber;
+    }
+
+    private static Parser<char[]> added =
+        from numbers in Parse.AtLeastOnce(Parse.Letter)
+        from _ in Parse.Optional(Parse.WhiteSpace.Then(_ => Parse.Char('+')).Then(_ => Parse.WhiteSpace))
+        select numbers.ToArray();
+
+    private static Parser<char[]> summed =
+        from _ in Parse.WhiteSpace.Then(_ => Parse.String("==")).Then(_ => Parse.WhiteSpace)
+        from numbers in Parse.AtLeastOnce(Parse.Letter)
+        select numbers.ToArray();
+
+    private static Parser<(char[][] added, char[] summed)> all =
+        from allAdded in Parse.Many(added)
+        from result in summed
+        select (allAdded.ToArray(), result);
+}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,8 +92,8 @@ namespace KataAlphameticsSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Alphametics("ZEROES + ONES = BINARY")); //"ZEROES + ONES = BINARY\" -> \"698392 + 3192 = 701584\"
-            //Console.WriteLine(Alphametics("SEND + MORE = MONEY"));
+            //Console.WriteLine(Alphametics("ZEROES + ONES = BINARY")); //"ZEROES + ONES = BINARY\" -> \"698392 + 3192 = 701584\"
+            Console.WriteLine(Alphametics("SEND + MORE = MONEY"));
             //Console.WriteLine(Alphametics("AD + BA = CE"));
             //Console.WriteLine(Alphametics("MA + MA = ABB")); //"MA + MA = ABB" >> 61 + 61 = 122
         }
@@ -77,34 +153,112 @@ namespace KataAlphameticsSolver
             CopyColumnCarrysToTestCarrys();
             testColumn = 0;
 
-            //TestLoop(testColumn);
 
             while (testColumn < result.Length)
             {
-                if (!AllValuesKnown(testColumn))
+                if (!AllValuesKnown(testColumn, testingDict))
                 {
                     Console.WriteLine($"There are unknown values in column {testColumn}\n");
                     ConstrainColumn(testColumn);
-
-                    CopyTestingDictionaryToColumnRevertDictionary(testColumn);
-                    TryValuesForColumn(testColumn);
-                }
-                testColumn++;
-                ShowMeWhatYouGot();
-                if (testColumn == result.Length)
-                {
-                    Console.WriteLine("\n TRYING SUM");
-                    if (!SumWorks())
+                    if (AllAddendsKnown(testColumn))
                     {
-                        Console.WriteLine("Didn't work\n");
-                        CopyColumnRevertDictionaryToTestingDictionary(0);
-                        RemoveLowestUnknownFromColumnMainDict(0);
-                        testColumn = 0;
+                        CopyTestingDictionaryToColumnRevertDictionary(testColumn);
+                        Console.WriteLine($"We know all the addends for column {testColumn}");
+                        if (AllValuesKnown(testColumn + 1, testingDict))
+                        {
+                            if (testCarrys[testColumn] == true)
+                            {
+                                Console.WriteLine($"We know all the values for the next column and know this one ({testColumn}) requires a carry");
+                                if (testingDict[result[testColumn]].Contains(KnownAddendSumMod10(testColumn) + 1))
+                                {
+                                    Console.WriteLine("Setting result accordingly");
+                                    SetValForChar(testingDict, KnownAddendSumMod10(testColumn) + 1, result[testColumn]); // will need to sort for diff carry values
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Nice try, lets go again\n");
+                                    CopyColumnRevertDictionaryToTestingDictionary(0);
+                                    RemoveLowestUnknownFromColumnMainDict(0);
+                                    testColumn = 0;
+                                }
+                            }
+                            else
+                            {
+
+                                Console.WriteLine($"We know all the values for the next column and know this one don't require a carry");
+                                if (testingDict[result[testColumn]].Contains(KnownAddendSumMod10(testColumn)))
+                                {
+                                    Console.WriteLine("Setting result accordingly");
+                                    SetValForChar(testingDict, KnownAddendSumMod10(testColumn), result[testColumn]); // will need to sort for diff carry values
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Nice try, lets go again\n");
+                                    CopyColumnRevertDictionaryToTestingDictionary(0);
+                                    RemoveLowestUnknownFromColumnMainDict(0);
+                                    testColumn = 0;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        CopyTestingDictionaryToColumnRevertDictionary(testColumn);
+                        TryValuesForColumn(testColumn);
+                    }
+                }
+                else
+                {
+                    testColumn++;
+                    ShowMeWhatYouGot();
+                    if (testColumn == result.Length)
+                    {
+                        Console.WriteLine("\n TRYING SUM");
+                        if (!SumWorks())
+                        {
+                            Console.WriteLine("Didn't work\n");
+                            CopyColumnRevertDictionaryToTestingDictionary(0);
+                            RemoveLowestUnknownFromColumnMainDict(0);
+                            testColumn = 0;
+                        }
                     }
                 }
             }
 
             ShowMeWhatYouGot();
+        }
+
+        static int KnownAddendSumMod10(int column)
+        {
+            int addendSum = 0;
+            for (int i = 0; i < addends.Length; i++)
+            {
+                int globalIndex = (addends[i].Length - result.Length) + column;
+                if (globalIndex >= 0)
+                {
+                    char currentChar = addends[i][globalIndex];
+                    addendSum += testingDict[currentChar][0];
+                }
+            }
+            Console.WriteLine($"Known addend sum is {addendSum}");
+            return addendSum % 10;
+        }
+
+        static bool AllAddendsKnown(int column)
+        {
+            for (int i = 0; i < addends.Length; i++)
+            {
+                int globalIndex = (addends[i].Length - result.Length) + column;
+                if (globalIndex >= 0)
+                {
+                    char currentChar = addends[i][globalIndex];
+                    if (testingDict[currentChar].Count != 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
         static bool SumWorks()
@@ -296,7 +450,14 @@ namespace KataAlphameticsSolver
         {
             Console.WriteLine($"Trying again\n");
 
-            RemoveValuesThatWontWorkForChar(testingDict[lastCharWeGuessedAValueFor][0], lastCharWeGuessedAValueFor, column);
+            if (!RemoveValuesThatWontWorkForChar(testingDict[lastCharWeGuessedAValueFor][0], lastCharWeGuessedAValueFor, column))
+            {
+                CopyColumnRevertDictionaryToTestingDictionary(0);
+                RemoveLowestUnknownFromColumnMainDict(0);
+                testColumn = 0;
+                return;
+            }
+
 
             // if there are no values left to try
             if (testingDict[lastCharWeGuessedAValueFor].Count == 0) // will need adjusting for more than 2 addends
@@ -420,7 +581,7 @@ namespace KataAlphameticsSolver
             }
         }
 
-        static void RemoveValuesThatWontWorkForChar(int val, char ch, int column)
+        static bool RemoveValuesThatWontWorkForChar(int val, char ch, int column)
         {
             CopyCharRevertDictionaryToTestingDictionary();
 
@@ -432,6 +593,7 @@ namespace KataAlphameticsSolver
                     Console.WriteLine($"Removing {i} from possible values for {lastCharWeGuessedAValueFor}");
                 }
             }
+            return testingDict[lastCharWeGuessedAValueFor].Count > 0;
         }
 
         static void SetValForChar(Dictionary<char, List<int>> dict, int i, char ch)
@@ -442,12 +604,11 @@ namespace KataAlphameticsSolver
 
         static void ConstrainColumn(int column)
         {
+            Console.WriteLine($"Working out constraints for column {column}");
             var constraintTempDict = testingDict;
             if (column == 0)
             {
-                Console.WriteLine("Working out sonstraints for column 0");
-                // result[0] cannot be smaller than the sum of this columns minimum possible addends
-
+                Console.WriteLine($"Making sure column {column} of the result cannot be smaller than the its minimum possible addends");
                 int columnAddendsSum = 0;
                 for (int i = 0; i < addends.Length; i++)
                 {
@@ -462,6 +623,29 @@ namespace KataAlphameticsSolver
                 }
                 SetMinPossibleValFor(mainDict, columnAddendsSum, result[0]);
                 CopyMainDictionaryToTestingDictionary();
+            }
+
+            if (column < result.Length - 1)
+            {
+                if (AllValuesKnown(column + 1, testingDict))   // if we know everything about the next column we can be sure of some things for this column
+                {
+                    Console.WriteLine($"We know all the values for the next column ({column + 1}) Lets see if it can help us with this one ({column})");
+                    int columnAddendsSum = 0;
+                    for (int i = 0; i < addends.Length; i++)
+                    {
+                        int globalIndex = (addends[i].Length - result.Length) + column;
+                        if (globalIndex == column + 1)
+                        {
+                            char currentChar = addends[i][0];
+                            columnAddendsSum += testingDict[currentChar][0];
+                        }
+                    }
+                    if (columnAddendsSum > 9)
+                    {
+                        Console.WriteLine($"The next column ({column + 1}) must carry into this one ({column}), as its addends sum to more than 9");
+                        testCarrys[column] = true;
+                    }
+                }
             }
         }
 
@@ -496,32 +680,35 @@ namespace KataAlphameticsSolver
             }
         }
 
-        static bool AllValuesKnown(int column)
+        static bool AllValuesKnown(int column, Dictionary<char, List<int>> dict)
         {
             Console.WriteLine($"\nChecking column: {column}");
+            int addendSum = 0;
             for (int i = 0; i < addends.Length; i++)
             {
-                int addendColumnIndex = (addends[i].Length - result.Length) + column;
-                if(addendColumnIndex >= 0)
+                int globalIndex = (addends[i].Length - result.Length) + column;
+                if(globalIndex >= 0)
                 {
-                    if (!ValueCertain(addends[i][addendColumnIndex]))
+                    if (!ValueCertain(addends[i][globalIndex], dict))
                         return false;
+                    addendSum += addends[i][globalIndex];
                 }
             }
 
-            if (!ValueCertain(result[column]))
+            if (!ValueCertain(result[column], dict))
                 return false;
 
+            testCarrys[column] = addendSum % 10 != result[column]; // set carry if we know all values
             return true;
         }
 
-        static bool ValueCertain(char ch)
+        static bool ValueCertain(char ch, Dictionary<char, List<int>> dict)
         {
             Console.WriteLine($"Checking certainty for char: {ch}");
-            if (testingDict[ch].Count == 1)
+            if (dict[ch].Count == 1)
                 Console.WriteLine($"{ch} is {testingDict[ch][0]}");
 
-            return mainDict[ch].Count != 1 ? false : true;
+            return dict[ch].Count != 1 ? false : true;
         }
 
         static void ShowMeWhatYouGot()
